0b03dc3ec4b1173a85a3009da38d16e4becfb240
DomHolden <you@example.com>
2025-04-01 18:58:50 +0100

    add xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=C:\Users\domho\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS="--xformers"
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 
 call webui.bat
