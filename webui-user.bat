f856564313b0e1b3048b51d69db89875a16a048c
DomHolden <you@example.com>
2025-04-01 18:56:24 +0100

    optimisation

--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,6 @@ set PYTHON=C:\Users\domho\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 
 call webui.bat
